deaf1a38b7ac6fc292f2e4feee8638d9cab7af87
Alam <13517096@std.stei.itb.ac.id>
2019-11-02 11:18:07 +0700

    Finished mvcc_storage implementation

---
+++ txn/mvcc_storage_test.cc
@@ -0,0 +1,18 @@
+#include "mvcc_storage.h"
+
+
+
+
+int main(){
+
+    MVCCStorage* storage = new MVCCStorage();
+    storage->InitStorage();
+    Value v;
+    for (int i = 0; i < 10000;i++) {
+        if(storage->Read(i, &v, 0)){
+            cout << v << endl;
+        }
+
+    }
+    return 0;
+}
